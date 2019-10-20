ba5ec9a231d3c75d9f0b73ab93cd9859305b4010
Yang Zheng <ptyzheng@gmail.com>
2019-10-20 12:41:54 -0700

    starter code for python learners

---
+++ imageLoader.py
@@ -0,0 +1,14 @@
+import cv2
+import sys
+import numpy as np
+import face_recognition
+
+#read 
+def load_images:
+	face_encodings = []
+	face_names = []
+	#Code goes here
+
+	#return face_encodings, face_names
+
+	return face_encodings, face_names
